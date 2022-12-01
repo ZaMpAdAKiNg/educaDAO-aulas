b99e766912476b32db1e05cfd8c8ab227fd3a3a3
ZaMpAdAKiNg <119633199+ZaMpAdAKiNg@users.noreply.github.com>
2022-12-01 20:27:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+<style>
+        .container {
+            width: 70%;
+            height: 300px;
+            margin-left: 15%;
+            margin-right: 15%;
+            background: rgb(214, 214, 214);
+        } 
+        .campo1, .campo2, .operacao, .resultado {
+            width: 46%;
+            padding: 2%;
+        }  
+        .campo1, .operacao{
+            float: left;
+        }
+        .campo2, .resultado{
+            float: right;
+        }
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+            input{
+                width: 100%;
+            }
+            select{
+                width: 100%;
+                margin-top: 20px;
+            }
+        
+        </style>
